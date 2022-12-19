49d123ff14186fd49a5541c08cfb2d22f363eb0d
Hyojin <hyojin.ju@gmx.net>
2022-12-19 21:59:52 +0900

    Function declaration

---
+++ declare-function.js
@@ -0,0 +1,11 @@
+// 함수선언식
+function multiplyBy2(input1) {
+    input1 = input1 * 2;
+    return input1;
+}
+
+// 함수표현식
+let divideBy2 = function (input2) {
+    input2 = input2 / 2;
+    return input2;
+  }
